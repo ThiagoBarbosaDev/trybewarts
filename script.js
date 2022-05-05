d246154a1df61a092a894a27ee0be6d3e0a2a7bc
Icaro Joel <icaromoura.programador@gmail.com>
2022-05-05 16:36:20 -0300

    corrigindo mais erros

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 const buttonSubmit = document.querySelector('#submit-button');
-
 const inputPwd = document.querySelector('#pwd-input');
 const inputEmail = document.querySelector('#email-input');
 
 buttonSubmit.addEventListener('click', () => {
-  if (inputEmail.value === 'tryber@teste.com' && inputPwd.value == '123456') {
+  if (inputEmail.value === 'tryber@teste.com' && inputPwd.value === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos.');
