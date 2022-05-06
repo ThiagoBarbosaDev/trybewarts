0195192e24fd186d9e95b1ebe9afccf2eb9f37ca
Icaro Joel <icaromoura.programador@gmail.com>
2022-05-06 15:49:48 -0300

    eu acho que funfou o req 21

--- script.js
+++ script.js
@@ -28,38 +28,95 @@ textArea.addEventListener('input', () => {
   characterCounter.innerText = `${500 - textArea.value.length}`;
 });
 
-function get() {
-  const getName = document.getElementById('input-name');
-  const getLastName = document.getElementById('input-lastname');
-  const getEmail = document.getElementById('input-email');
+submitForm.addEventListener('click', (event) => {
+  event.preventDefault();
+});
+
+const result = [];
+const res = ['Nome:', 'Email:', 'Casa:', 'Família:', 'Matérias:', 'Avaliação:', 'observações:'];
+
+submitForm.addEventListener('click', () => {
+  const getName = document.getElementById('input-name').value;
+  const getLastName = document.getElementById('input-lastname').value;
+  const fullName = `${getName} ${getLastName}`;
+
+  result.push(fullName);
+});
+
+submitForm.addEventListener('click', () => {
+  const getEmail = document.getElementById('input-email').value;
+
+  result.push(getEmail);
+});
+
+submitForm.addEventListener('click', () => {
   const getHouse = document.getElementById('house');
-  const getFamily = document.querySelector('radio-family');
+  const house = getHouse.children[getHouse.selectedIndex].text;
 
-  const fullName = getName + getLastName;
-  const house = getHouse.options[getHouse.selectedIndex].text;
-  let family;
+  result.push(house);
+});
 
-  for (let i = 0; i < getFamily.length; i += 1) {
-    if (getFamily[i].checked) {
-      family = getFamily[i].value;
+submitForm.addEventListener('click', () => {
+  const getFam = document.querySelectorAll('.family');
+  let fam;
+
+  for (let i = 0; i < getFam.length; i += 1) {
+    if (getFam[i].checked) {
+      fam = getFam[i].value;
     }
   }
 
-  const array = [fullName, getEmail, house, family];
-  return array;
-}
+  result.push(fam);
+});
+
+submitForm.addEventListener('click', () => {
+  const sub = document.querySelectorAll('.subject');
+  let subs = '';
+
+  for (let i = 0; i < sub.length; i += 1) {
+    if (sub[i].checked) {
+      subs += `${sub[i].value} `;
+    }
+  }
+
+  result.push(subs);
+});
+
+submitForm.addEventListener('click', () => {
+  const rate = document.querySelectorAll('.rate');
+  let not;
+
+  for (let i = 0; i < rate.length; i += 1) { if (rate[i].checked) { not = rate[i].value; } }
+
+  result.push(not);
+});
+
+submitForm.addEventListener('click', () => {
+  const textarea = document.getElementById('textarea').value;
+
+  result.push(textarea);
+});
 
 submitForm.addEventListener('click', () => {
   const form = document.getElementById('evaluation-form');
   form.innerHTML = '';
-  const res = ['Nome:', 'Email:', 'Casa:', 'Família:', 'Matérias:', 'Avaliação:', 'observações:'];
-  const array = get();
+});
 
-  for (let i = 0; i < res.length; i += 1) {
+submitForm.addEventListener('click', () => {
+  const form = document.getElementById('evaluation-form');
+  const div = document.createElement('div');
+  div.className = 'container-result';
+
+  form.appendChild(div);
+});
+
+submitForm.addEventListener('click', () => {
+  const divContainer = document.querySelector('.container-result');
+
+  for (let i = 0; i < result.length; i += '') {
     const p = document.createElement('p');
-    p.className = 'result';
-    p.innerHTML = res[i] + array[i];
+    p.innerHTML = `${res[i]} ${result[i]}`;
 
-    form.appendChild(p);
+    divContainer.appendChild(p);
   }
 });
